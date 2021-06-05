24c51b3e2f1dc847fa7b94e40359bb5f78f46a61
ogoogo <ogosae.1018173@icloud.com>
2021-06-05 19:12:00 +0900

    fix:イイコトがない時のバグを修正

--- app/src/main/java/com/pico/ogoshi/feelingrecorder/SavedActivity.kt
+++ app/src/main/java/com/pico/ogoshi/feelingrecorder/SavedActivity.kt
@@ -3,6 +3,7 @@ package com.pico.ogoshi.feelingrecorder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import io.realm.Realm
 import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_saved.*
@@ -28,22 +29,28 @@ class SavedActivity : AppCompatActivity() {
         }
 
         if(goodOrBad==false) {
-
             val goodfeelings: RealmResults<Memo> = readGood()
-            val randomGood = goodfeelings[Random.nextInt(goodfeelings.size)]
-            val goodText :String = randomGood?.event.toString()
-            val goodYear:String = randomGood?.year.toString()
-            val goodMonth:String = randomGood?.month.toString()
-            val goodDate :String = randomGood?.date.toString()
-            val goodPerson:String = randomGood?.personName.toString()
-            val goodQUote:String = randomGood?.quote.toString()
-            val goodQuoteOrNot = randomGood?.quoteOrNot
+            if (goodfeelings.isEmpty()){
+                messageTextView.text="おつかれさまです！\nきっと明日はイイコトありますよ！！"
 
-            if(goodQuoteOrNot==false) {
-                messageTextView.text = "${goodYear}年${goodMonth}月${goodDate}日には、こんなことがあったみたいですよ！\n${goodText}"
-            }else if(goodQuoteOrNot==true){
-                messageTextView.text = "${goodYear}年${goodMonth}月${goodDate}日には\n${goodPerson}に「\n${goodQUote}」と言われました!!"
+            }else{
+                val randomGood = goodfeelings[Random.nextInt(goodfeelings.size)]
+                val goodText :String = randomGood?.event.toString()
+                val goodYear:String = randomGood?.year.toString()
+                val goodMonth:String = randomGood?.month.toString()
+                val goodDate :String = randomGood?.date.toString()
+                val goodPerson:String = randomGood?.personName.toString()
+                val goodQUote:String = randomGood?.quote.toString()
+                val goodQuoteOrNot = randomGood?.quoteOrNot
+
+                if(goodQuoteOrNot==false) {
+                    messageTextView.text = "${goodYear}年${goodMonth}月${goodDate}日には、こんなことがあったみたいですよ！\n${goodText}"
+                }else if(goodQuoteOrNot==true){
+                    messageTextView.text = "${goodYear}年${goodMonth}月${goodDate}日には\n${goodPerson}に「\n${goodQUote}」と言われました!!"
+                }
+
             }
+
         }else{
             if(quoteOrNot) {
                 val quote:String?=intent.getStringExtra("quote")
